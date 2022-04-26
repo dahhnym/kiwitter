278a3902827fd7f5cf32e87352986b2c1a8ec3d0
Danim Kim <kdn725@gmail.com>
2022-04-27 01:16:08 +0900

    ✨ add OAuth feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
 import AppRouter from "./Router";
+import { useEffect, useState } from "react";
+import { authService } from "fbase";
+import Navigation from "./Navigation";
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
+  // setInterval(() => {
+  //   console.log(authService.currentUser)
+  // }, 2000);
   return (
-    <AppRouter />
+    <>
+      {isLoggedIn && <Navigation />}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy;Kiwitter {new Date().getFullYear() }</footer>
+    </>
   );
 }
 
