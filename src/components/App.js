b69a5c020a0eab1d18b4976d0abdf7d89b9a4d89
Danim Kim <kdn725@gmail.com>
2022-04-28 18:21:13 +0900

    🗃️ firebase firestore setting to get user data

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,34 @@
-import AppRouter from "./Router";
-import { useEffect, useState } from "react";
-import { authService } from "fbase";
-import Navigation from "./Navigation";
+import AppRouter from './Router';
+import { useEffect, useState } from 'react';
+import { authService } from 'fbase';
+import Navigation from './Navigation';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
-  }, [])
-  // setInterval(() => {
-  //   console.log(authService.currentUser)
-  // }, 2000);
+  }, []);
+
   return (
     <>
       {isLoggedIn && <Navigation />}
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
-      <footer>&copy;Kiwitter {new Date().getFullYear() }</footer>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        'Initializing...'
+      )}
+      <footer>&copy;Kiwitter {new Date().getFullYear()}</footer>
     </>
   );
 }
